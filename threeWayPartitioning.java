0132546da2b0e2cb95ad8c0b1574521fae50d7df
Hariom <imhariomr@gmail.com>
2024-09-07 19:48:23 +0530

    Update threeWayPartitioning.java

--- threeWayPartitioning.java
+++ threeWayPartitioning.java
@@ -1,32 +1,40 @@
-public class threeWayPartitioning {
-    public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 3, 4 };
-        int a = 1;
-        int b = 2;
-
+public class ThreeWayPartitioning {
+    public static void threeWayPartition(int arr[], int a, int b) {
         int start = 0;
+        int mid = 0;
         int end = arr.length - 1;
 
-        while (start < end) {
-
-            if (arr[start] > b) {
+        while (mid <= end) {
+            if (arr[mid] < a) {
+                // Elements less than a should be moved to the beginning
                 int temp = arr[start];
-                arr[start] = arr[end];
+                arr[start] = arr[mid];
+                arr[mid] = temp;
+                start++;
+                mid++;
+            } else if (arr[mid] > b) {
+                // Elements greater than b should be moved to the end
+                int temp = arr[mid];
+                arr[mid] = arr[end];
                 arr[end] = temp;
                 end--;
-
-            } else if (arr[end] < a) {
-                int temp = arr[end];
-                arr[end] = arr[start];
-                arr[start] = temp;
-                start++;
             } else {
-                start++;
+                // Elements between a and b should remain in the middle
+                mid++;
             }
         }
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+    public static void main(String[] args) {
+        int arr[] = {1, 14, 5, 20, 4, 2, 54, 20, 87, 98, 3, 1, 32};
+        int a = 14;
+        int b = 20;
+
+        threeWayPartition(arr, a, b);
+
+        // Print the partitioned array
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
